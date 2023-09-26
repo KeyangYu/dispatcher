4600755e734828b9a4eb4bf8b006aac844cf83f2
KeyangYu <kyu009@fiu.edu>
2023-09-26 14:57:21 -0600

    .

--- coordinator.py
+++ coordinator.py
@@ -4,13 +4,11 @@ from xmlrpc.server import SimpleXMLRPCRequestHandler
 from sklearn.datasets import fetch_openml
 from threading import Thread
 
-
-# Subclass SimpleXMLRPCServer to capture client address
-class MyXMLRPCServer(SimpleXMLRPCServer):
-    def _dispatch(self, method, params):
-        self.current_client_address = self.RequestHandlerClass.client_address
-        return super()._dispatch(method, params)
-
+# Subclass the RequestHandler to capture client address in the server
+class CustomXMLRPCRequestHandler(SimpleXMLRPCRequestHandler):
+    def do_POST(self):
+        self.server.client_address = self.client_address
+        super().do_POST()
 
 # Load MNIST data
 mnist = fetch_openml('mnist_784')
@@ -18,51 +16,36 @@ mnist = fetch_openml('mnist_784')
 app = Flask(__name__)
 
 # State information
-nodes = {}  # Format: {node_id: {'status': 'working/finished'}}
+nodes = {}
 
-
-class RequestHandler(SimpleXMLRPCRequestHandler):
-    rpc_paths = ('/RPC2',)
-
-
-# Create server using the subclassed MyXMLRPCServer
-server = MyXMLRPCServer(('0.0.0.0', 9090), requestHandler=RequestHandler)
+# Create server using the custom request handler
+server = SimpleXMLRPCServer(('0.0.0.0', 9090), requestHandler=CustomXMLRPCRequestHandler)
 server.register_introspection_functions()
-
 
 # Define a function to serve MNIST data
 def get_mnist():
-    # Get client IP address from the overridden _dispatch
-    node_id = server.current_client_address[0]
-
-    # Register the node and set its status to 'working'
+    # Get client IP address from the server
+    node_id = server.client_address[0]
     nodes[node_id] = {'status': 'working'}
-
     return mnist.data.tolist(), mnist.target.tolist()
 
-
 server.register_function(get_mnist, 'get_mnist')
-
 
 # Define a function to mark the training as finished for a node
 def mark_training_complete(node_id):
     nodes[node_id]['status'] = 'finished'
     return True
 
-
 server.register_function(mark_training_complete, 'mark_training_complete')
-
 
 @app.route('/')
 def index():
     return render_template('status.html', nodes=nodes)
 
-
 # Background thread for XMLRPC server
 def run_rpc_server():
     print("Dispatcher RPC Server is running...")
     server.serve_forever()
-
 
 if __name__ == '__main__':
     rpc_thread = Thread(target=run_rpc_server)
