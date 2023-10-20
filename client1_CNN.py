470cdcf99591c67681d4329f21c46386280f082d
KeyangYu <kyu009@fiu.edu>
2023-10-19 23:36:03 -0600

    .

---
+++ client1_CNN.py
@@ -0,0 +1,69 @@
+import xmlrpc.client
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+from sklearn.metrics import accuracy_score
+import time
+from xmlrpc.server import SimpleXMLRPCServer
+
+CLIENT_PORT = 9091  # Unique port for each client. Replace X with the client number.
+
+proxy = xmlrpc.client.ServerProxy("http://localhost:9090/RPC2")
+
+def wait_for_start_signal():
+    print("Waiting for coordinator to start training...")
+    while True:
+        if proxy.should_start_training():
+            return True
+        time.sleep(5)  # Poll every 5 seconds
+
+client_server = SimpleXMLRPCServer(('localhost', CLIENT_PORT))
+client_server.register_introspection_functions()
+
+# Register any additional functions for client_server if needed
+
+def run_client_server():
+    client_server.serve_forever()
+
+def build_cnn_model(input_shape, num_classes):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+    return model
+
+if __name__ == "__main__":
+    # Starting the XML-RPC server in the background
+    import threading
+
+    thread = threading.Thread(target=run_client_server)
+    thread.start()
+
+    while True:
+        if wait_for_start_signal():
+            # Fetch the MNIST data
+            print("Fetching MNIST data from Dispatcher...")
+            data, target = proxy.get_mnist(f"clientX")  # Replace X with the client number
+
+            # Preprocess the data for CNN
+            data = np.array(data).reshape(-1, 28, 28, 1)
+            target = to_categorical(np.array(target))
+
+            model = build_cnn_model((28, 28, 1), 10)
+            model.fit(data, target, epochs=10, batch_size=128, verbose=1)
+
+            # Save the trained model to a file
+            model_name = f"clientX_model.h5"  # Replace X with the client number
+            model.save(model_name)
+
+            # Send the model back to the coordinator
+            with open(model_name, "rb") as model_file:
+                proxy.upload_model(xmlrpc.client.Binary(model_file.read()), f"clientX")  # Replace X with the client number
+
+            print("Model training completed and sent to the coordinator.")
+            break
--- coordinator_CNN.py
+++ coordinator_CNN.py
@@ -3,8 +3,13 @@ from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 from sklearn.datasets import fetch_openml
 from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score
+from keras.models import load_model
+import numpy as np
 from threading import Thread
 import pandas as pd
+import joblib
+import os
 
 class CustomXMLRPCRequestHandler(SimpleXMLRPCRequestHandler):
     def do_POST(self):
@@ -28,18 +33,16 @@ client_ranks = load_client_ranks_from_csv('rank.csv')
 
 shuffled_data, shuffled_target = shuffle(mnist.data, mnist.target, random_state=42)
 
-
 def get_mnist(client_id):
     node_id = server.client_address[0]
     nodes[node_id] = {'status': 'working'}
-    proportion = client_ranks.get(client_id, 0.2)  # Default to 20% if rank not found
+    proportion = client_ranks.get(client_id, 0.2)
 
     total_data_size = len(shuffled_data)
     start_index = int((client_ranks[client_id] - 0.2) * total_data_size)
     end_index = start_index + int(proportion * total_data_size)
 
-    return shuffled_data.iloc[start_index:end_index].values.tolist(), shuffled_target.iloc[
-                                                                      start_index:end_index].values.tolist()
+    return shuffled_data.iloc[start_index:end_index].values.tolist(), shuffled_target.iloc[start_index:end_index].values.tolist()
 
 server.register_function(get_mnist, 'get_mnist')
 
@@ -49,15 +52,44 @@ def mark_training_complete(node_id):
 
 server.register_function(mark_training_complete, 'mark_training_complete')
 
+models_received = 0
+
 def upload_model(model_binary, client_id):
-    filename = f"{server.client_address[0]}_{client_id}_model.pkl"
+    global models_received
+    filename = f"{client_id}_model.h5"
     with open(filename, 'wb') as f:
         f.write(model_binary.data)
     print(f"Model from {client_id} dumped in dispatcher as {filename}.")
+
+    models_received += 1
+    if models_received == 3:
+        ensemble_and_evaluate()
+
     return True
 
-
 server.register_function(upload_model, 'upload_model')
+
+def ensemble_and_evaluate():
+    models = []
+    for client_id in ["client1", "client2", "client3"]:
+        model_name = f"{client_id}_model.h5"
+        if os.path.exists(model_name):
+            models.append(load_model(model_name))
+        else:
+            print(f"Model for {client_id} not found.")
+            return
+
+    X_test = np.array(shuffled_data.iloc[int(0.7 * len(shuffled_data)):].values.tolist()).reshape(-1, 28, 28, 1)
+    y_test = np.array(shuffled_target.iloc[int(0.7 * len(shuffled_data)):].values.tolist())
+
+    # Majority vote
+    predictions = []
+    for x in X_test:
+        preds = [np.argmax(model.predict(x.reshape(1, 28, 28, 1))) for model in models]
+        predictions.append(max(set(preds), key=preds.count))
+
+    # Print evaluation metrics
+    print(f"Accuracy: {accuracy_score(y_test, predictions)}")
 
 def should_start_training():
     return training_started
