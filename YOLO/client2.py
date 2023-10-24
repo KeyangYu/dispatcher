9c0e22df0327e006d1a3c0e27fcf59d00507238b
KeyangYu <kyu009@fiu.edu>
2023-10-24 10:20:15 -0600

    YOLO not finished yet. CNN added 2 methods: avg/stack

---
+++ YOLO/client2.py
@@ -0,0 +1,69 @@
+import xmlrpc.client
+import time
+import threading
+from xmlrpc.server import SimpleXMLRPCServer
+import os
+import numpy as np
+from ensemble_yolo import load_yolo_for_human_detection, train_yolo, yolo_predict
+
+CLIENT_PORT = 9092
+
+proxy = xmlrpc.client.ServerProxy("http://localhost:9090/RPC2")
+
+
+def wait_for_start_signal():
+    print("Waiting for coordinator to start training...")
+    while True:
+        try:
+            if proxy.should_start_training():
+                return True
+            time.sleep(5)
+        except Exception as e:
+            print(f"Error while waiting for start signal: {e}")
+            time.sleep(10)
+
+
+client_server = SimpleXMLRPCServer(('localhost', CLIENT_PORT))
+client_server.register_introspection_functions()
+
+
+def run_client_server():
+    client_server.serve_forever()
+
+
+if __name__ == "__main__":
+    thread = threading.Thread(target=run_client_server)
+    thread.start()
+
+    while True:
+        if wait_for_start_signal():
+            print("Start signal received. Starting training...")
+            try:
+                # Load human data
+                print("Fetching data for training...")
+                train_images, train_annotations = proxy.get_human_data(f"client2")
+
+                # Load YOLO model for human detection
+                print("Loading YOLO model...")
+                model = load_yolo_for_human_detection()
+
+                # Train YOLO on human data
+                print("Training YOLO model...")
+                train_yolo(model, train_images, train_annotations)
+
+                # Save the trained model
+                print("Saving trained model...")
+                model_name = f"client2_model.h5"
+                model.save(model_name)
+
+                # Send the model to the coordinator
+                print("Sending model to coordinator...")
+                with open(model_name, "rb") as model_file:
+                    proxy.upload_model(xmlrpc.client.Binary(model_file.read()), f"client2")
+
+                print("Model training completed and sent to the coordinator.")
+                break
+
+            except Exception as e:
+                print(f"Error during training or sending the model: {e}")
+                time.sleep(10)
---
+++ YOLO/coordinator.py
@@ -0,0 +1,196 @@
+from sklearn.datasets import fetch_openml
+from sklearn.utils import shuffle
+from threading import Thread
+import joblib
+import os
+from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
+from flask import Flask
+import pandas as pd
+from threading import Thread
+import numpy as np
+from ensemble_yolo import ensemble_detections
+from ensemble_yolo import load_yolo_for_human_detection, train_yolo, yolo_predict
+import cv2
+
+
+class CustomXMLRPCRequestHandler(SimpleXMLRPCRequestHandler):
+    def do_POST(self):
+        self.server.client_address = self.client_address
+        super().do_POST()
+
+
+app = Flask(__name__)
+nodes = {}
+server = SimpleXMLRPCServer(('0.0.0.0', 9090), requestHandler=CustomXMLRPCRequestHandler)
+server.register_introspection_functions()
+
+training_started = False
+
+
+def load_client_ranks_from_csv(file_name):
+    df = pd.read_csv(file_name)
+    ranks = dict(zip(df['client_id'], df['rank']))
+    return ranks
+
+
+client_ranks = load_client_ranks_from_csv('rank.csv')
+
+
+def get_human_data(client_id):
+    node_id = server.client_address[0]
+    nodes[node_id] = {'status': 'working'}
+
+    proportion = client_ranks.get(client_id, 0.2)
+
+    # You will need to modify the lines below to load the WIDER Person dataset instead of COCO
+    total_data_size = len(wider_images) # Assuming wider_images holds the paths to WIDER Person images
+
+    start_index = int((client_ranks[client_id] - 0.2) * total_data_size)
+    end_index = start_index + int(proportion * total_data_size)
+
+    # Get a subset of images and annotations based on the calculated indices
+    selected_images = wider_images[start_index:end_index]
+    selected_annotations = wider_annotations[start_index:end_index]
+
+    return selected_images, selected_annotations
+
+# Register the function with the server
+server.register_function(get_human_data, 'get_human_data')
+
+
+def mark_training_complete(node_id):
+    nodes[node_id]['status'] = 'finished'
+    return True
+
+
+server.register_function(mark_training_complete, 'mark_training_complete')
+
+
+def preprocess_for_yolo(frame, size=416):
+    """
+    Preprocess the image frame for YOLO model input.
+
+    :param frame: Input frame.
+    :param size: Size to which the frame should be resized.
+    :return: Preprocessed frame.
+    """
+    # Resize the frame
+    resized_frame = cv2.resize(frame, (size, size))
+
+    # Normalize the pixel values
+    normalized_frame = resized_frame.astype('float32') / 255.0
+
+    # Add batch dimension
+    input_frame = np.expand_dims(normalized_frame, axis=0)
+
+    return input_frame
+
+
+def draw_bboxes(frame, detections):
+    """
+    Draw bounding boxes on a given frame.
+
+    :param frame: The frame on which to draw.
+    :param detections: The detections for this frame.
+    :return: The frame with bounding boxes drawn.
+    """
+    for detection in detections:
+        # Assuming each detection is a tuple (x_min, y_min, x_max, y_max)
+        cv2.rectangle(frame, (detection[0], detection[1]), (detection[2], detection[3]), (0, 255, 0), 2)
+    return frame
+
+
+def yolo_predict_and_draw(model, video_path, output_video_path):
+    cap = cv2.VideoCapture(video_path)
+
+    # Set up video writer
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_video_path, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+
+    all_detections = []
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Preprocess the frame for YOLO model input
+        processed_frame = preprocess_for_yolo(frame)
+
+        # Get detections for the current frame
+        detections = model.predict(processed_frame)  # Modify based on your YOLO model's API
+        all_detections.append(detections)
+
+        # Draw bounding boxes on the frame
+        frame_with_bboxes = draw_bboxes(frame, detections)
+
+        # Write the frame with bounding boxes to the output video
+        out.write(frame_with_bboxes)
+
+    cap.release()
+    out.release()
+    return all_detections
+
+
+models_received = 0
+
+def upload_model(model_binary, client_id):
+    global models_received
+    filename = f"{client_id}_model.h5"
+    with open(filename, 'wb') as f:
+        f.write(model_binary.data)
+    print(f"Model from {client_id} dumped in dispatcher as {filename}.")
+
+    models_received += 1
+    if models_received == 3:
+        ensemble_and_evaluate_yolo()
+
+    return True
+
+server.register_function(upload_model, 'upload_model')
+
+def ensemble_and_evaluate_yolo():
+    model_files = [f"client{i}_model.h5" for i in range(1, 4)]
+    test_video = "test_1.mp4"
+
+    detections_from_all_models = []
+
+    for i, model_file in enumerate(model_files):
+        model = load_yolo_for_human_detection(model_file)  # Placeholder to load your YOLO model
+        output_video_path = f"output_{i}.mp4"  # Name of the video with bounding boxes for this model
+        detections = yolo_predict_and_draw(model, test_video, output_video_path)
+        detections_from_all_models.append(detections)
+
+    final_detections = ensemble_detections(detections_from_all_models)
+    # Evaluate the ensemble against the ground truth (like mAP, IoU, etc.)
+
+
+def should_start_training():
+    return training_started
+
+
+server.register_function(should_start_training, 'should_start_training')
+
+
+@app.route('/')
+def index():
+    return "Coordinator is running"
+
+
+def run_rpc_server():
+    global training_started
+    print("Dispatcher RPC Server is running...")
+
+    while True:
+        start = input("Enter 'start' to begin training on all clients: ")
+        if start.strip().lower() == 'start':
+            training_started = True
+            break
+
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    rpc_thread = Thread(target=run_rpc_server)
+    rpc_thread.start()
+    app.run(host='0.0.0.0', port=5050)
